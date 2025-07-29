a7a56cc8261869a367526c32b63208652c78ac06
Elias Häußler <e.haeussler@familie-redlich.de>
2025-07-29 11:22:17 +0200

    [TASK] Add packaging_exclude.php file for TER releases

---
+++ packaging_exclude.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS extension "cps_myra_cloud".
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+return [
+    'directories' => [
+        '.build',
+        '.git',
+        '.github',
+        'bin',
+        'build',
+        'public',
+        'tailor-version-upload',
+        'tests',
+        'vendor',
+    ],
+    'files' => [
+        'DS_Store',
+        'composer.lock',
+        'composer-dependency-analyser.php',
+        'docker-compose.yaml',
+        'editorconfig',
+        'gitattributes',
+        'gitignore',
+        'packaging_exclude.php',
+        'php-cs-fixer.php',
+        'phpunit.xml',
+        'rector.php',
+        'version-bumper.yaml',
+    ],
+];
