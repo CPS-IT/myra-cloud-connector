eba2542f64fb5f95a88c7c6c9d032e1cc55d9d65
Elias Häußler <e.haeussler@familie-redlich.de>
2025-10-06 11:19:18 +0200

    [TASK] Include all root-level PHP files in Rector migration

--- rector.php
+++ rector.php
@@ -29,8 +29,7 @@ return RectorConfig::configure()
         __DIR__ . '/Classes',
         __DIR__ . '/Configuration',
         __DIR__ . '/Tests',
-        __DIR__ . '/ext_emconf.php',
-        __DIR__ . '/ext_tables.php',
+        __DIR__ . '/*.php',
     ])
     ->withPhpSets(php82: true)
     ->withPhpVersion(PhpVersion::PHP_82)
